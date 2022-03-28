59782a3b5e933ee0c3cb7436a7995f3707279f6a
João Pedro Parisi <joaopparisi@gmail.com>
2022-03-28 11:00:34 -0300

    Criação do Projeto

---
+++ exemplo.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE dbEscola1
+USE dbEscola1
+
+CREATE TABLE tbCurso(
+    Id_Curso INT PRIMARY KEY,
+    Nome_Curso VARCHAR(50)
+)
+
+CREATE TABLE tbProfessor(
+    Rm_Professor INT PRIMARY KEY,
+    Nome_Professor VARCHAR(50)
+)
